d31889fc410bd6281cb98e24c8dde9abce0ebb9a
JulielJ <julieljunior@hotmail.com>
2022-02-16 21:46:19 -0300

    primeiro commit calculadora

---
+++ calculadora/src/br/com/juliel/calc/visao/Display.java
@@ -0,0 +1,29 @@
+package br.com.juliel.calc.visao;
+
+
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+	
+	private JLabel label ;
+	
+	public Display() {
+		setBackground(new Color(49, 49, 49));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		
+		add(label);
+		
+		
+	}
+}
